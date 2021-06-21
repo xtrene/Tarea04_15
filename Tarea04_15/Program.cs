c053292ab9a60d63854bc69ab113ffb419a9509f
Carlos <carlos@xtrene.com>
2021-06-21 23:38:37 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarea04_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce un número:  ");
+            int  numero = Convert.ToInt32( Console.ReadLine() );
+
+            if (numero%2 == 0)
+            {
+                Console.WriteLine("El número {0} es divisible entre 2", numero);
+            }
+            else
+            {
+                Console.WriteLine("El número {0} NO es divisible entre 2", numero);
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
